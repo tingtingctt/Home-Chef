1e42ab68271a641f41c955d7695e552d91530054
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-05 13:47:30 -0500

    got ingredient api functioning

--- assets/recipe.js
+++ assets/recipe.js
@@ -9,6 +9,22 @@ $(".ingredBtn").on("click", function(event) {
 
     console.log(ingredientInput);
 
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://recipe-puppy.p.rapidapi.com/?p=1&i=" + ingredientInput,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+            "x-rapidapi-key": "c12fbcd6b6mshb18e26a60bb432cp1f2c4fjsn82a51563e64c"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        var results = JSON.parse(response);
+        console.log(results);
+        }
+    });
 
 
 });
