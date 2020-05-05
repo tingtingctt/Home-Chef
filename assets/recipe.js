2e7ce5c7ad530ff9eb2b874714218357ff7011d1
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-05 14:03:41 -0500

    added functionality and api to url button

--- assets/recipe.js
+++ assets/recipe.js
@@ -79,3 +79,29 @@ $("#saveBtn").on("click", function(event) {
 
 
 });
+
+$(".urlBtn").on("click", function(event) {
+    event.preventDefault();
+    var submittedURL = $(".inputURL").val()
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi",
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
+            "x-rapidapi-key": "c12fbcd6b6mshb18e26a60bb432cp1f2c4fjsn82a51563e64c",
+            "content-type": "text/plain",
+            "accept": "text/plain"
+        },
+        "data": submittedURL
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+
+
+
+});
