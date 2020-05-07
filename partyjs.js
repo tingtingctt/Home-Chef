6b8953d0954c4c4c8d13b1d624d6268c527e2f10
jcmastropieri <jcmastro5@gmail.com>
2020-05-07 11:54:05 -0700

    button adjustment

--- partyjs.js
+++ partyjs.js
@@ -2,9 +2,11 @@ $("#submitGrocery").on('click', function(){
     let grocery = $("#groceryInput").val().trim();
     if(grocery){
          $("#listContainer").append(`<div class="row mt-1 mb-1"><input type="checkbox" class="ml-3 mr-3">
-    <label for="vehicle3"><h3>${grocery}</h3></label><button class="btn btn-success ml-4 btn-sm">Complete</button>
-    <button class="ml-4 btn btn-danger btn-sm">Delete</button></div>`)
+    <label for="vehicle3"><h3>${grocery}</h3></label><button class="btn btn-success ml-9 btn-sm">Complete</button>
+    <button class="ml-3 btn btn-danger btn-sm">Delete</button></div>`)
     }
 
+
+    
 })
 
